9c76d99606a0bae325572f0e47c6a790f2797915
larsbirger <larsbirger.bergmaal@gmail.com>
2022-09-10 02:38:43 +0200

    added a basic PID controller

--- Arduino-scripts/iBot-Frans-drivers/PID.cpp
+++ Arduino-scripts/iBot-Frans-drivers/PID.cpp
@@ -1,18 +1,111 @@
-#include "Arduino.h"
 #include "PID.h"
 
-PID::PID(
-    signed double kP = 0.0,
-    signed double kI = 0.0,
-    signed double kD = 0.0
-)
+PID::PID(double Kp, double Ki, double Kd, double tau, double limMin, double limMax, double T)
 {
-    this._kP = kP;
-    this._kI = kI;
-    this._kD = kD;
-}
-        
-PID::set(signed double setValue)
+    //sets gains
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    //sets filter time
+    this->tau = tau;
+
+    //sets limits
+    this->limMin = limMin;
+    this->limMax = limMax;
+    
+    //sets sampling time
+    this->T = T;
+    
+    //asigns emptu memory so we dont get "not defined" value error bs
+    double integrator = 0.0;
+    double preverror = 0.0;
+    double differentiator = 0.0;
+    double prevMeassurement = 0.0;
+};
+
+double PID::update(double setPoint, double meassurement)
 {
-    this._setValue = setValue;
-}
+    /*gets error. the error is always posetively setpoint - meassurement so that when you do the feedback loop in state space you do negative gain for stability.*/
+    double error = setPoint - meassurement; //the error between the set value and the meassurment
+
+
+
+
+    /* gets the proportional part of the output value */
+    double proportional = this->Kp * error; //the proportional part of the output value
+
+
+
+
+    /* accumulates the integrator */
+    this->integrator += 0.5 * this->Ki * this->T * (error + this->prevError); //the discretly accumulated integrator
+
+
+    /*making sure the integrator doesnt saturate the output value */
+    float limMinInt; // mimimum value for the integrator
+    float limMaxInt; // maximum value for the integrator
+
+    if (this->limMax > proportional)
+    {
+        limMaxInt = this->limMax - proportional;
+    }
+    else
+    {
+        limMaxInt = 0.0;
+    }
+    
+    if (this->limMin < proportional)
+    {
+        limMinInt = this->limMin - proportional;
+    }
+    else
+    {
+        limMinInt = 0.0;
+    }
+    
+    if (this->integrator > limMaxInt)
+    {
+        this->integrator = limMaxInt;
+    }
+    else if (this->integrator < limMinInt)
+    {
+        this->integrator = limMinInt;
+    }
+
+
+
+
+    /* sets the differentiator */
+    this->differentiator = ( 2.0 * this->Kd * (meassurement - this->prevMeassurement)   /*note we are deriving on meassurement and not error.*/ 
+                            + ( 2.0 * this->tau - this->T ) * this->differentiator )    /*this is mostly because we can, but also some bart due to*/
+                            / ( 2.0 *this->tau + this->T );                             /*beneficial behavior of the system*/
+    /*also note that we are using a low pass filter on ther differentiator, this is to have a way of dealing with noise*/
+
+
+
+
+    /*summing shit together*/
+    this->Out = proportional + this->integrator + this->differentiator;
+
+
+    /*limiting the output value*/
+    if (this->Out > this->limMax)
+    {
+        this->Out = this->limMax;
+    }
+    else if (this->Out < this->limMin)
+    {
+        this->Out=this->limMin;
+    }
+    
+    /*keeping error*/
+    this->prevError = error;
+
+    /*keeping the meassurement*/
+    this->prevMeassurement = meassurement;
+
+
+    /*FINALLY we can return the one thing we actually wanted.... the output*/
+    return this->Out;
+};
--- Arduino-scripts/iBot-Frans-drivers/PID.h
+++ Arduino-scripts/iBot-Frans-drivers/PID.h
@@ -1,27 +1,35 @@
 #ifndef PID_h
-
 #define PID_h
-#include "Arduino.h"
-
-
 
 class PID
 {
-    private:
-        signed double _kP;
-        signed double _kI;
-        signed double _kd;
-        unsigned double _setValue;
-
     public:
-        PID(
-            signed double kP = 0.0,
-            signed double kI = 0.0,
-            signed double kD = 0.0
-        );
-        updateOn(unsigned double getValue);
-        set(unsigned double setValue);
-        get();
-        currentError();
-}
+        /*controller gains */
+        double Kp; // propotrional gain
+        double Ki; // inegrator gain
+        double Kd; // derivator gain
+
+        /* derivator lowpass filter time constant (in seconds) */
+        double tau; // the time constant
+
+        /* output limits */
+        double limMin; //minimum output value
+        double limMax; //maximum output value
+
+        // sample time (in seconds)
+        double T; //small t for continuous time, large T for discrete time
+
+        /* controller "memory" */
+        double integrator; //integrating part of the output value
+        double prevError; //the previous error
+        double differentiator; //the differentiating part of the output value
+        double prevMeassurement; //the previous meassurement
+
+        //Controller output
+        double Out;
+
+        double update(double setPoint, double meassurement);
+        PID(double Kp, double Ki, double Kd, double tau, double limMin, double limMax, double T);
+};
+
 #endif
