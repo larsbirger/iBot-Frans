89b1cf75c5db87ed2d0e659c435652f4b0844618
larsbirger <larsbirger.bergmaal@gmail.com>
2022-09-11 15:28:51 +0200

    implementet protothreading

--- Arduino-scripts/iBot-Frans/iBot-Frans.ino
+++ Arduino-scripts/iBot-Frans/iBot-Frans.ino
@@ -11,15 +11,27 @@ Encoder encoderB(6,7);
 void setup()
 {
     Serial.begin(57600);//Initialize the serial port
+    PT_INIT(&ptEncoder)
 }
 
 void loop()
 {
-    Serial.print("PulseA:");
-    String msg = (String) encoderA.getDuration();
-    Serial.print(msg);
-    Serial.print(", PulseB:");
-    msg = (String) encoderB.getDuration();
-    Serial.println(msg);
-    delay(100);
+    PT_SCHEDULE(encoderThread(&ptEncoder));
+}
+
+pt ptEncoder;
+int encoderThread(struct pt* pt)
+{
+    PT_BEGIN(pt);
+    for (;;)
+    {
+        Serial.print("PulseA:");
+        String msg = (String) encoderA.getDuration();
+        Serial.print(msg);
+        Serial.print(", PulseB:");
+        msg = (String) encoderB.getDuration();
+        Serial.println(msg);
+        PT_SLEEP(pt,100);
+    }
+    PT_END(pt);
 }
